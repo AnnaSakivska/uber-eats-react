d052df45a0f3574d8cbaac1209b41fd5e17c8465
Anna Sakivska <annasakivska@gmail.com>
2019-07-08 17:26:27 +0300

    new changes

--- src/components/Restaurant-card/Restaurant-card.jsx
+++ src/components/Restaurant-card/Restaurant-card.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./restaurant-card.css";
+import "./Restaurant-card.css";
 
 export default function RestaurantsCard(props) {
   return (
-    <li className="restaurant-card">
+    <li className="Restaurant-card">
       <Link to="/restaurant">
         <img
-          className="restaurant-card__image-wrapper"
+          className="Restaurant-card__image-wrapper"
           src={props.imageUrl}
           alt="image of food restaurant"
         />
-        <div className="restaurant-card__description">
-          <h2 className="restaurant-card__name">{props.title}</h2>
-          <span className="restaurant-card__average-price">
+        <div className="Restaurant-card__description">
+          <h2 className="Restaurant-card__name">{props.title}</h2>
+          <span className="Restaurant-card__average-price">
             {props.priceBucket} •{" "}
             {props.categories
               .map(category => {
@@ -21,7 +21,7 @@ export default function RestaurantsCard(props) {
               })
               .join(" • ")}
           </span>
-          <h4 className="restaurant-card__cooking-time">
+          <h4 className="Restaurant-card__cooking-time">
             {props.etaRange.min} - {props.etaRange.max} Min
           </h4>
         </div>
