8702204a484a32cd03d6de8c64bba50e1cc3ee44
Anna Sakivska <annasakivska@gmail.com>
2019-07-09 23:32:38 +0300

    few changes

--- src/components/RestaurantLabel/RestaurantLabel.jsx
+++ src/components/RestaurantLabel/RestaurantLabel.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import "./RestaurantLabel.css";
-import {Container} from "../Container/Container.jsx";
+import { Container } from "../Container/Container.jsx";
 
 export default function RestaurantLabel() {
-    return (
-        <div className="restaurant-label__wrapper">
-            <Container>
-                <div className="Restaurant-label">
-                    <h2 className="restaurant-label__name">Трактир «Пушкин»</h2>
-                    <div className="restaurant-label__information">
-                        <h3 className="restaurant-label__description">₽₽₽ • Европейская</h3>
-                        <span className="restaurant-label__delivery">40 - 50 Min</span>
-                    </div>
-                </div>
-            </Container>
+  return (
+    <div className="restaurant-label__wrapper">
+      <Container>
+        <div className="Restaurant-label">
+          <h2 className="restaurant-label__name">Трактир «Пушкин»</h2>
+          <div className="restaurant-label__information">
+            <h3 className="restaurant-label__description">₽₽₽ • Европейская</h3>
+            <span className="restaurant-label__delivery">40 - 50 Min</span>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 }
--- src/components/RestaurantPage/RestaurantPage.jsx
+++ src/components/RestaurantPage/RestaurantPage.jsx
@@ -3,17 +3,20 @@ import "../../App.css";
 import RestaurantLabel from "../RestaurantLabel/RestaurantLabel.jsx";
 import RestaurantMenu from "../RestaurantMenu/RestaurantMenu.jsx";
 import RestaurantNav from "../RestaurantNav/RestaurantNav.jsx";
+import {Container} from "../Container/Container";
 
 export function RestaurantPage() {
-  return (
-    <main className="Restaurant-page">
-      <section className="restaurant-page__header">
-        <RestaurantLabel />
-      </section>
-      <RestaurantNav />
-      <RestaurantMenu />
-    </main>
-  );
+    return (
+        <main className="Restaurant-page">
+            <section className="restaurant-page__header">
+                <RestaurantLabel/>
+            </section>
+            <RestaurantNav/>
+            <Container>
+                <RestaurantMenu/>
+            </Container>
+        </main>
+    );
 }
 
 export default RestaurantPage;
