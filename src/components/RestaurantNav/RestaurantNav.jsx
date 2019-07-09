aac01f1f2e56357174f8984a2b266e5672b16a98
Anna Sakivska <annasakivska@gmail.com>
2019-07-09 23:47:04 +0300

    some changes

--- src/components/RestaurantNav/RestaurantNav.jsx
+++ src/components/RestaurantNav/RestaurantNav.jsx
@@ -1,45 +1,45 @@
 import React from "react";
 import "./RestaurantNav.css";
-import {Container} from "../Container/Container";
+import { Container } from "../Container/Container";
 
 export default function RestaurantNav() {
-    return (
-        <>
-            <Container>
-                <ul className="Restaurant-nav">
-                    <li className="restaurant-nav__option">
-                        <a className="restaurant-nav__option-link" href="#">
-                            Закуски
-                        </a>
-                    </li>
-                    <li className="restaurant-nav__option">
-                        <a className="restaurant-nav__option-link" href="#">
-                            Салаты
-                        </a>
-                    </li>
-                    <li className="restaurant-nav__option">
-                        <a className="restaurant-nav__option-link" href="#">
-                            Супы
-                        </a>
-                    </li>
-                    <li className="restaurant-nav__option">
-                        <a className="restaurant-nav__option-link" href="#">
-                            Горячие блюда
-                        </a>
-                    </li>
-                    <li className="restaurant-nav__option">
-                        <a className="restaurant-nav__option-link" href="#">
-                            Гарниры
-                        </a>
-                    </li>
-                    <li className="restaurant-nav__option">
-                        <a className="restaurant-nav__option-link" href="#">
-                            Десерты
-                        </a>
-                    </li>
-                </ul>
-            </Container>
-            <hr className="restaurant-nav__bottom-line"/>
-        </>
-    );
+  return (
+    <>
+      <Container>
+        <ul className="Restaurant-nav">
+          <li className="restaurant-nav__option">
+            <a className="restaurant-nav__option-link" href="#">
+              Закуски
+            </a>
+          </li>
+          <li className="restaurant-nav__option">
+            <a className="restaurant-nav__option-link" href="#">
+              Салаты
+            </a>
+          </li>
+          <li className="restaurant-nav__option">
+            <a className="restaurant-nav__option-link" href="#">
+              Супы
+            </a>
+          </li>
+          <li className="restaurant-nav__option">
+            <a className="restaurant-nav__option-link" href="#">
+              Горячие блюда
+            </a>
+          </li>
+          <li className="restaurant-nav__option">
+            <a className="restaurant-nav__option-link" href="#">
+              Гарниры
+            </a>
+          </li>
+          <li className="restaurant-nav__option">
+            <a className="restaurant-nav__option-link" href="#">
+              Десерты
+            </a>
+          </li>
+        </ul>
+      </Container>
+      <hr className="restaurant-nav__bottom-line" />
+    </>
+  );
 }
