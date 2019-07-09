26befb5064ebba94927dcd67f9134c92693f5522
Anna Sakivska <annasakivska@gmail.com>
2019-07-09 12:34:03 +0300

    normal hover effect #16

--- src/components/Restaurant/restaurant-nav/Restaurant-nav.jsx
+++ src/components/Restaurant/restaurant-nav/Restaurant-nav.jsx
@@ -3,37 +3,37 @@ import "./restaurant-nav.css";
 
 export default function RestaurantNav() {
   return (
-      <ul className="restaurant-nav">
-        <li className="restaurant-nav__option">
-          <a className="restaurant-nav__option-link" href="#">
-            Закуски
-          </a>
-        </li>
-        <li className="restaurant-nav__option">
-          <a className="restaurant-nav__option-link" href="#">
-            Салаты
-          </a>
-        </li>
-        <li className="restaurant-nav__option">
-          <a className="restaurant-nav__option-link" href="#">
-            Супы
-          </a>
-        </li>
-        <li className="restaurant-nav__option">
-          <a className="restaurant-nav__option-link" href="#">
-            Горячие блюда
-          </a>
-        </li>
-        <li className="restaurant-nav__option">
-          <a className="restaurant-nav__option-link" href="#">
-            Гарниры
-          </a>
-        </li>
-        <li className="restaurant-nav__option">
-          <a className="restaurant-nav__option-link" href="#">
-            Десерты
-          </a>
-        </li>
-      </ul>
+    <ul className="restaurant-nav">
+      <li className="restaurant-nav__option">
+        <a className="restaurant-nav__option-link" href="#">
+          Закуски
+        </a>
+      </li>
+      <li className="restaurant-nav__option">
+        <a className="restaurant-nav__option-link" href="#">
+          Салаты
+        </a>
+      </li>
+      <li className="restaurant-nav__option">
+        <a className="restaurant-nav__option-link" href="#">
+          Супы
+        </a>
+      </li>
+      <li className="restaurant-nav__option">
+        <a className="restaurant-nav__option-link" href="#">
+          Горячие блюда
+        </a>
+      </li>
+      <li className="restaurant-nav__option">
+        <a className="restaurant-nav__option-link" href="#">
+          Гарниры
+        </a>
+      </li>
+      <li className="restaurant-nav__option">
+        <a className="restaurant-nav__option-link" href="#">
+          Десерты
+        </a>
+      </li>
+    </ul>
   );
 }
