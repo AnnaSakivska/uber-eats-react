60bd06ee7a61c24f73762302c897d7f55209e2c7
Anna Sakivska <annasakivska@gmail.com>
2019-07-09 13:27:01 +0300

    some changes

--- src/components/Restaurant/restaurant-nav/Restaurant-nav.jsx
+++ src/components/Restaurant/restaurant-nav/Restaurant-nav.jsx
@@ -3,29 +3,29 @@ import "./restaurant-nav.css";
 
 export default function RestaurantNav() {
   return (
-    <ul className="restaurant-nav">
-      <li className="restaurant-nav__option">
-        <a className="restaurant-nav__option-link" href="#">
+    <ul className="Restaurant-nav">
+      <li className="Restaurant-nav__option">
+        <a className="Restaurant-nav__option-link" href="#">
           Закуски
         </a>
       </li>
-      <li className="restaurant-nav__option">
-        <a className="restaurant-nav__option-link" href="#">
+      <li className="Restaurant-nav__option">
+        <a className="Restaurant-nav__option-link" href="#">
           Салаты
         </a>
       </li>
-      <li className="restaurant-nav__option">
-        <a className="restaurant-nav__option-link" href="#">
+      <li className="Restaurant-nav__option">
+        <a className="Restaurant-nav__option-link" href="#">
           Супы
         </a>
       </li>
-      <li className="restaurant-nav__option">
-        <a className="restaurant-nav__option-link" href="#">
+      <li className="Restaurant-nav__option">
+        <a className="Restaurant-nav__option-link" href="#">
           Горячие блюда
         </a>
       </li>
-      <li className="restaurant-nav__option">
-        <a className="restaurant-nav__option-link" href="#">
+      <li className="Restaurant-nav__option">
+        <a className="Restaurant-nav__option-link" href="#">
           Гарниры
         </a>
       </li>
