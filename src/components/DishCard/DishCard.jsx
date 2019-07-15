49fe79cea42e0ea5a0b16ecbcc24c626589ba50f
Anna Sakivska <annasakivska@gmail.com>
2019-07-15 12:17:44 +0300

    new changes

--- src/components/DishCard/DishCard.jsx
+++ src/components/DishCard/DishCard.jsx
@@ -3,23 +3,26 @@ import "./DishCard.css";
 import { restaurantMenuInform } from "../restaurantMenuInform/restaurantMenuInform";
 
 export function DishCard(props) {
-  const {title, itemDescription, price, imageUrl} = restaurantMenuInform.items[props.id];
+  const {
+    title,
+    itemDescription,
+    price,
+    imageUrl
+  } = restaurantMenuInform.items[props.id];
 
   return (
     <div className="Dish-card">
       <div className="dish-card__description">
-        <span className="dish-card__title">
-          {title}
-        </span>
-        <span className="dish-card__ingredients">
-          {itemDescription}
-        </span>
-        <span className="dish-card__price">
-          {price / 100}₽
-        </span>
+        <span className="dish-card__title">{title}</span>
+        <span className="dish-card__ingredients">{itemDescription}</span>
+        <span className="dish-card__price">{price / 100}₽</span>
       </div>
       <div className="dish-card__picture-wrapper">
-        {imageUrl && <img className="dish-card__picture" src={imageUrl} alt="Dish" />}
+        {imageUrl !== undefined && (
+          <img className="dish-card__picture"
+               src={imageUrl}
+               alt="Dish" />
+        )}
       </div>
     </div>
   );
